4cd6d00a8fb0f975b0666ee059b7885d239042af
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2014-10-26 12:10:58 +0100

    feat : crud volumes & ports mapping

--- public/modules/services/controllers/services.client.controller.js
+++ public/modules/services/controllers/services.client.controller.js
@@ -1,79 +1,101 @@
 'use strict';
 
 angular.module('services').controller('ServicesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Services',
-	function($scope, $stateParams, $location, Authentication, Services) {
-		$scope.authentication = Authentication;
+    function ($scope, $stateParams, $location, Authentication, Services) {
+        $scope.authentication = Authentication;
 
-		$scope.create = function() {
-			var service = new Services({
-				title: this.title,
-				content: this.content
-			});
-			service.$save(function(response) {
-				$location.path('services/' + response._id);
+        $scope.displayFormImage = false;
+        $scope.volume = {};
+        $scope.displayFormVolume = false;
+        $scope.port = {};
+        $scope.displayFormPort = false;
 
-				$scope.title = '';
-				$scope.content = '';
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+        $scope.create = function () {
+            var service = new Services({
+                title: this.title,
+                content: this.content
+            });
+            service.$save(function (response) {
+                $location.path('services/' + response._id);
 
-		$scope.remove = function(service) {
-			if (service) {
-				service.$remove();
+                $scope.title = '';
+                $scope.content = '';
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-				for (var i in $scope.services) {
-					if ($scope.services[i] === service) {
-						$scope.services.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.service.$remove(function() {
-					$location.path('services');
-				});
-			}
-		};
+        $scope.remove = function (service) {
+            if (service) {
+                service.$remove();
 
-		$scope.update = function() {
-			var service = $scope.service;
-
-			service.$update(function() {
-				$location.path('services/' + service._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
-		$scope.find = function() {
-			$scope.services = Services.query();
-		};
-
-		$scope.findOne = function() {
-			$scope.service = Services.get({
-				serviceId: $stateParams.serviceId
-			});
-		};
-
-
-        $scope.removeImage = function(imgId) {
-            for (var i = 0; i < $scope.service.images.length; i++) {
-                if ($scope.service.images[i]._id === imgId) {
-                    $scope.service.images.splice(i, 1);
+                for (var i in $scope.services) {
+                    if ($scope.services[i] === service) {
+                        $scope.services.splice(i, 1);
+                    }
                 }
+            } else {
+                $scope.service.$remove(function () {
+                    $location.path('services');
+                });
             }
         };
 
-        $scope.addImage = function(isValid){
-            if (isValid) {
-                $scope.service.images.push({
-                    name: $scope.imageName,
-                    active: true
-                });
+        $scope.update = function () {
+            var service = $scope.service;
+
+            service.$update(function () {
+                $location.path('services/' + service._id);
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        $scope.find = function () {
+            $scope.services = Services.query();
+        };
+
+        $scope.findOne = function () {
+            $scope.service = Services.get({
+                serviceId: $stateParams.serviceId
+            });
+        };
+
+        $scope.removeImage = function (row) {
+            var index = $scope.service.images.indexOf(row);
+            if (index !== -1) {
+                $scope.service.images.splice(index, 1);
             }
+        };
+
+        $scope.addImage = function () {
+            $scope.service.images.push({
+                name: $scope.imageName,
+                active: true,
+                volumes: [],
+                ports: []
+            });
             $scope.imageName = '';
             $scope.imageIsActive = true;
         };
+
+        $scope.addVolume = function (image) {
+            image.volumes.push($scope.volume);
+            $scope.volume = {};
+        };
+
+        $scope.removeVolume = function (image, volume) {
+            image.volumes.splice(image.volumes.indexOf(volume), 1);
+        };
+
+        $scope.addPort = function (image) {
+            image.ports.push($scope.port);
+            $scope.port = {};
+        };
+
+        $scope.removePort = function (image, port) {
+            image.ports.splice(image.ports.indexOf(port), 1);
+        }
 
     }
 ]);
