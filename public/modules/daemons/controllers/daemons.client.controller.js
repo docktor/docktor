d68b16c4a136b6de7944fe03c3f9fb8a18deaf89
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2014-11-09 15:58:11 +0100

    refactor

--- public/modules/daemons/controllers/daemons.client.controller.js
+++ public/modules/daemons/controllers/daemons.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('daemons').controller('DaemonsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Daemons', 'DaemonsDocker',
-    function ($scope, $stateParams, $location, Authentication, Daemons, DaemonsDocker) {
+angular.module('daemons').controller('DaemonsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Daemons', 'Daemon',
+    function ($scope, $stateParams, $location, Authentication, Daemons, Daemon) {
         $scope.authentication = Authentication;
 
         $scope.create = function () {
@@ -66,69 +66,7 @@ angular.module('daemons').controller('DaemonsController', ['$scope', '$statePara
                 $scope.daemons = daemons;
                 angular.forEach($scope.daemons, function (daemon, key) {
                     daemon.cadvisorUrl = daemon.cadvisorApi.substring(0, daemon.cadvisorApi.indexOf('/api'));
-                    daemon.dockerStatus = 'checking';
-
-                    DaemonsDocker.info(daemon._id).
-                        success(function (info) {
-                            daemon.dockerInfo = info;
-                            daemon.dockerStatus = 'up';
-
-                            DaemonsDocker.version(daemon._id).
-                                success(function (version) {
-                                    daemon.dockerVersion = version;
-                                })
-                                .error(function (resp) {
-                                    console.log('Error with DaemonsDocker.version on :' + daemon._id + ':' + resp);
-                                });
-                            DaemonsDocker.machineInfo(daemon._id).
-                                success(function (machineInfo) {
-                                    daemon.machineInfo = machineInfo;
-                                    daemon.stats = {};
-
-                                    DaemonsDocker.statsDaemon(daemon._id).
-                                        success(function (daemonInfo, status, headers, config) {
-                                            daemonInfo.stats = daemonInfo.stats;
-
-                                            var cur = daemonInfo.stats[daemonInfo.stats.length - 1];
-                                            var cpuUsage = 0;
-                                            if (daemonInfo.spec.has_cpu && daemonInfo.stats.length >= 2) {
-                                                var prev = daemonInfo.stats[daemonInfo.stats.length - 2];
-                                                var rawUsage = cur.cpu.usage.total - prev.cpu.usage.total;
-                                                var intervalInNs = DaemonsDocker.getInterval(cur.timestamp, prev.timestamp);
-                                                // Convert to millicores and take the percentage
-                                                cpuUsage = Math.round(((rawUsage / intervalInNs) / daemon.machineInfo.num_cores) * 100);
-                                                if (cpuUsage > 100) {
-                                                    cpuUsage = 100;
-                                                }
-                                            }
-                                            daemon.stats.cpuUsagePercent = cpuUsage;
-
-                                            if (daemonInfo.spec.has_memory) {
-                                                // Saturate to the machine size.
-                                                var limit = daemonInfo.spec.memory.limit;
-                                                if (limit > daemon.machineInfo.memory_capacity) {
-                                                    limit = daemon.machineInfo.memory_capacity;
-                                                }
-                                                daemon.stats.memoryLimit = limit;
-                                                daemon.stats.memoryUsage = Math.round(cur.memory.usage / 1000000);
-                                                daemon.stats.memoryUsagePercent = Math.round((cur.memory.usage / limit) * 100);
-                                            }
-                                        }).
-                                        error(function (data, status, headers, config) {
-                                            console.log('Error:');
-                                            console.log(data);
-                                        });
-
-                                })
-                                .error(function (resp) {
-                                    console.log('Error with DaemonsDocker.machineInfo:' + resp);
-                                });
-                        })
-                        .error(function (resp) {
-                            daemon.dockerStatus = 'down';
-                            console.log('Error with DaemonsDocker.info on :' + daemon._id + ':' + resp);
-                        });
-
+                    Daemon.getDetails(daemon);
                 });
             });
         };
@@ -138,16 +76,7 @@ angular.module('daemons').controller('DaemonsController', ['$scope', '$statePara
                 daemonId: $stateParams.daemonId
             }, function (daemon) {
                 $scope.daemon = daemon;
-                $scope.daemon.dockerStatus = 'checking';
-                DaemonsDocker.info($stateParams.daemonId).
-                    success(function (info) {
-                        $scope.dockerInfo = info;
-                        $scope.daemon.dockerStatus = 'up';
-                    }).
-                    error(function (resp, status, headers, config) {
-                        $scope.daemon.dockerStatus = 'down';
-                        console.log('Error with DaemonsDocker.info:' + resp);
-                    });
+                Daemon.getDetails(daemon);
             });
         };
     }
---
+++ public/modules/daemons/services/daemon.client.service.js
@@ -0,0 +1,72 @@
+'use strict';
+
+angular.module('daemons').factory('Daemon', ['DaemonsDocker',
+    function (DaemonsDocker) {
+        return {
+            getDetails: function (daemon) {
+                daemon.dockerStatus = 'checking';
+
+                DaemonsDocker.info(daemon._id).
+                    success(function (info) {
+                        daemon.dockerInfo = info;
+                        daemon.dockerStatus = 'up';
+
+                        DaemonsDocker.version(daemon._id).
+                            success(function (version) {
+                                daemon.dockerVersion = version;
+                            })
+                            .error(function (resp) {
+                                console.log('Error with DaemonsDocker.version on :' + daemon._id + ':' + resp);
+                            });
+                        DaemonsDocker.machineInfo(daemon._id).
+                            success(function (machineInfo) {
+                                daemon.machineInfo = machineInfo;
+                                daemon.stats = {};
+
+                                DaemonsDocker.statsDaemon(daemon._id).
+                                    success(function (daemonInfo, status, headers, config) {
+                                        daemonInfo.stats = daemonInfo.stats;
+
+                                        var cur = daemonInfo.stats[daemonInfo.stats.length - 1];
+                                        var cpuUsage = 0;
+                                        if (daemonInfo.spec.has_cpu && daemonInfo.stats.length >= 2) {
+                                            var prev = daemonInfo.stats[daemonInfo.stats.length - 2];
+                                            var rawUsage = cur.cpu.usage.total - prev.cpu.usage.total;
+                                            var intervalInNs = DaemonsDocker.getInterval(cur.timestamp, prev.timestamp);
+                                            // Convert to millicores and take the percentage
+                                            cpuUsage = Math.round(((rawUsage / intervalInNs) / daemon.machineInfo.num_cores) * 100);
+                                            if (cpuUsage > 100) {
+                                                cpuUsage = 100;
+                                            }
+                                        }
+                                        daemon.stats.cpuUsagePercent = cpuUsage;
+
+                                        if (daemonInfo.spec.has_memory) {
+                                            // Saturate to the machine size.
+                                            var limit = daemonInfo.spec.memory.limit;
+                                            if (limit > daemon.machineInfo.memory_capacity) {
+                                                limit = daemon.machineInfo.memory_capacity;
+                                            }
+                                            daemon.stats.memoryLimit = limit;
+                                            daemon.stats.memoryUsage = Math.round(cur.memory.usage / 1000000);
+                                            daemon.stats.memoryUsagePercent = Math.round((cur.memory.usage / limit) * 100);
+                                        }
+                                    }).
+                                    error(function (data, status, headers, config) {
+                                        console.log('Error:');
+                                        console.log(data);
+                                    });
+
+                            })
+                            .error(function (resp) {
+                                console.log('Error with DaemonsDocker.machineInfo:' + resp);
+                            });
+                    })
+                    .error(function (resp) {
+                        daemon.dockerStatus = 'down';
+                        console.log('Error with DaemonsDocker.info on :' + daemon._id + ':' + resp);
+                    });
+            }
+        };
+    }
+]);
