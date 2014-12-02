ec4a16b909656915a74f098beeb420dc74c28bd3
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2014-12-03 00:16:28 +0100

    feat : refacto menu
    ref #20

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -3,16 +3,11 @@
 // Configuring the Sites module
 angular.module('sites').run(['Menus',
     function (Menus) {
-
         Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown', '/admin(.*)?');
         Menus.addSubMenuItem('topbar', 'admin', 'List Users', 'admin/users');
-
         Menus.addSubMenuItem('topbar', 'admin', 'List Daemons', 'admin/daemons');
-
         Menus.addSubMenuItem('topbar', 'admin', 'List Sites', 'admin/sites');
-
         Menus.addSubMenuItem('topbar', 'admin', 'List Services', 'admin/services');
-
         Menus.addSubMenuItem('topbar', 'admin', 'New Group', 'admin/groups/create');
     }
 ]);
--- public/modules/groups/config/groups.client.config.js
+++ public/modules/groups/config/groups.client.config.js
@@ -3,14 +3,7 @@
 // Configuring the Groups module
 angular.module('groups').run(['Menus',
     function (Menus) {
-        // Set top bar menu items
-
         Menus.addMenuItem('topbar', 'Groups', 'groups', 'dropdown', '/groups/?');
-        Menus.addSubMenuItem('topbar', 'groups', 'List Groups', 'groups')
-
-
-        /*Menus.addMenuItem('topbar', 'Groups', 'groups', 'dropdown', '/groups(/create)?');
-         Menus.addSubMenuItem('topbar', 'groups', 'List Groups', 'groups');
-         Menus.addSubMenuItem('topbar', 'groups', 'New Group', 'groups/create');*/
+        Menus.addSubMenuItem('topbar', 'groups', 'List Groups', 'groups');
     }
 ]);
