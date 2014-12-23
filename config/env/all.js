d6c04918c0c21913a4ce2a239362d67db02cbc51
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2014-12-23 21:32:59 +0100

    style : remove angular ui-select

--- config/env/all.js
+++ config/env/all.js
@@ -17,7 +17,6 @@ module.exports = {
                 'public/lib/angular-material/themes/grey-theme.css',
                 'public/lib/angular-material/angular-material.css',
                 'public/lib/fontawesome/css/font-awesome.css',
-                'public/lib/angular-ui-select/dist/select.css',
                 'public/lib/trNgGrid/release/trNgGrid.min.css'
             ],
             js: [
@@ -33,7 +32,6 @@ module.exports = {
                 'public/lib/angular-sanitize/angular-sanitize.js',
                 'public/lib/angular-ui-router/release/angular-ui-router.js',
                 'public/lib/angular-ui-utils/ui-utils.js',
-                'public/lib/angular-ui-select/dist/select.js',
                 'public/lib/trNgGrid/release/trNgGrid.min.js',
                 'public/lib/lodash/dist/lodash.underscore.min.js',
                 'public/lib/moment/min/moment.min.js'
