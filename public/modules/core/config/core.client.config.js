b1ded5c491afdf3132c03fbfe0504c9e89fa18a0
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2015-01-05 21:56:37 +0100

    feat : Breadcrumb on admin pages

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -4,10 +4,10 @@
 angular.module('sites').run(['Menus',
     function (Menus) {
         Menus.addMenuItem('topbar', 'Admin', 'admin', 'dropdown', '/admin(.*)?', null, ['admin']);
-        Menus.addSubMenuItem('topbar', 'admin', 'List Users', 'admin/users');
-        Menus.addSubMenuItem('topbar', 'admin', 'List Daemons', 'admin/daemons');
-        Menus.addSubMenuItem('topbar', 'admin', 'List Sites', 'admin/sites');
-        Menus.addSubMenuItem('topbar', 'admin', 'List Services', 'admin/services');
+        Menus.addSubMenuItem('topbar', 'admin', 'Users', 'admin/users');
+        Menus.addSubMenuItem('topbar', 'admin', 'Daemons', 'admin/daemons');
+        Menus.addSubMenuItem('topbar', 'admin', 'Sites', 'admin/sites');
+        Menus.addSubMenuItem('topbar', 'admin', 'Services', 'admin/services');
         Menus.addSubMenuItem('topbar', 'admin', 'New Group', 'admin/groups/create');
     }
 ]);
