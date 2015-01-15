d3542330665491e030d02cc3285b0b90363dd44d
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2015-01-15 13:41:52 +0100

    feat : work on job
    close #29

--- config/scheduler.js
+++ config/scheduler.js
@@ -241,6 +241,7 @@ module.exports.activateJob = function (job, serviceId, cbSuccess, cbError) {
             } else {
                 console.log('no deployed container, no schedule');
             }
+            cbSuccess();
         }
     });
 };
