5da3ba0c21a2a693d75b2bbaa15f623622b34170
Leclerc Gwendal <gwendal.leclerc@soprasteria.com>
2016-09-13 19:54:51 +0200

    feat: add some functions on volumeBox including validation + fix some scss

--- client/src/common/volumes.box.component.js
+++ client/src/common/volumes.box.component.js
@@ -10,40 +10,33 @@ class VolumesBox extends React.Component {
 
   constructor(props) {
     super(props);
+
     // Set state of component from the props.
     this.state = { volumes : this.props.volumes ? this.props.volumes : [] };
   }
 
-  renderVolume(volume, index) {
-          return (
-            <div className='fields' title={'-v ' + volume.value + ':' + volume.internal + ':' + volume.rights}>
-              <div className='five wide field'>
-                <input type='text' name='name' defaultValue={volume.value} placeholder='The default volume when container is created' autoComplete='off'/>
-              </div>
-              <div className='five wide field'>
-                <input type='text' name='name' defaultValue={volume.internal} placeholder='The volume inside the container' autoComplete='off'/>
-              </div>
-              <div className='three wide field'>
-                <select name='rights' defaultValue={volume.rights} ref='rights' className='ui fluid dropdown rights'  onChange={(event) => this.onChangeRights(event)}>
-                  <option value=''>Select rights</option>
-                  <option value='ro'>Read-only</option>
-                  <option value='rw'>Read-write</option>
-                </select>
-              </div>
-              <div className='three wide field'>
-                <textarea rows='2' name='description' defaultValue={volume.description} placeholder='Describe the utility of this volume' autoComplete='off'/>
-              </div>
-              <div className='one wide field'>
-                <button className='ui red icon button'>
-                  <i className='trash icon'></i>
-                </button>
-              </div>
-            </div>
-          );
+  componentDidMount() {
+    $('.volume .ui.dropdown.rights').dropdown();
+    this.refreshForm();
+  }
+
+  componentDidUpdate() {
+    $('.volume .ui.dropdown.rights').dropdown();
+    this.refreshForm();
+  }
+
+  refreshForm() {
+    const settings = { fields:{} };
+    this.state.volumes.forEach((volume, index) => {
+      settings.fields['external' + index] = 'empty';
+      settings.fields['internal' + index] = 'empty';
+      settings.fields['rights' + index] = 'empty';
+    });
+    $('.volume.ui.form').form(settings);
   }
 
   onAddVolume(event) {
-    console.log(event);
+    event.preventDefault();
     const volumes = this.state.volumes;
     volumes.push({
       value: '',
@@ -51,21 +44,76 @@ class VolumesBox extends React.Component {
       rights: 'rw',
       description: ''
     });
-    this.setState({ volumes: volumes });
+    this.forceUpdate();
   }
+
+  onRemoveVolume(event, index) {
+    event.preventDefault();
+    this.state.volumes.splice(index, 1);
+    this.forceUpdate();
+  }
+
+  onChangeVolume(event, index, property) {
+    event.preventDefault();
+    this.state.volumes[index][property] = event.target.value;
+    this.forceUpdate();
+  }
+
+  isFormValid() {
+    $('.volume.ui.form').form('validate form');
+    return $('.volume.ui.form').form('is valid');
+  }
+
+  renderVolume(volume, index) {
+          const title = '-v ' + volume.value + ':' + volume.internal + ':' + volume.rights;
+          return (
+            <div key={'volume' + index} className='fields'>
+              <div className='five wide field'>
+                <label className='hidden'>External Volume</label>
+                <input title={title} type='text' value={volume.value} placeholder='The default volume when container is created' autoComplete='off'
+                    onChange={(event) => this.onChangeVolume(event, index, 'value')} data-validate={'external' + index}/>
+              </div>
+              <div className='five wide field required'>
+                <label className='hidden'>Internal Volume</label>
+                <input title={title} type='text' value={volume.internal} placeholder='The volume inside the container' autoComplete='off'
+                    onChange={(event) => this.onChangeVolume(event, index, 'internal')} data-validate={'internal' + index} />
+              </div>
+              <div className='three wide field required' title={title}>
+                <label className='hidden'>Rights</label>
+                <select value={volume.rights} className='ui fluid dropdown rights'
+                    onChange={(event) => this.onChangeVolume(event, index, 'rights')} data-validate={'rights' + index} >
+                  <option value=''>Select rights</option>
+                  <option value='ro'>Read-only</option>
+                  <option value='rw'>Read-write</option>
+                </select>
+              </div>
+              <div className='three wide field'>
+                <label className='hidden'>Description</label>
+                <textarea rows='2' defaultValue={volume.description} placeholder='Describe the utility of this volume' autoComplete='off'
+                  onChange={(event) => this.onChangeVolume(event, index, 'description')} />
+              </div>
+              <div className='button field'>
+                <button className='ui red icon button' onClick={(event) => this.onRemoveVolume(event, index)}>
+                  <i className='trash icon'></i>
+                </button>
+              </div>
+            </div>
+          );
+  }
+
 
   render() {
     return (
-      <HeadingBox className='volume' icon='large folder open icon' title='Volumes'>
+      <HeadingBox className='volume ui form' icon='large folder open icon' title='Volumes'>
         {this.props.children ? this.props.children : <div></div>}
         <div className='fields header'>
           <div className='five wide field'>
             <label>External Volume</label>
           </div>
-          <div className='five wide field'>
+          <div className='five wide field required'>
             <label>Internal Volume</label>
           </div>
-          <div className='three wide field'>
+          <div className='three wide field required'>
             <label>Rights</label>
           </div>
           <div className='three wide field'>
--- client/src/daemons/daemon/daemon.page.js
+++ client/src/daemons/daemon/daemon.page.js
@@ -1,6 +1,7 @@
 // React
 import React from 'react';
 import { connect } from 'react-redux';
+import { goBack } from 'react-router-redux';
 
 import { fetchDaemon } from './daemon.thunks.js';
 import { fetchSitesIfNeeded } from '../../sites/sites.thunks.js';
@@ -20,7 +21,7 @@ class DaemonComponent extends React.Component {
   componentWillMount() {
     const daemonId = this.props.daemonId;
     this.props.fetchSites();
-    if (typeof daemonId !== 'undefined' && daemonId !== '') {
+    if (daemonId && daemonId !== '') {
       // Fetch when known daemon
       this.props.fetchDaemon(daemonId);
     } else {
@@ -30,16 +31,19 @@ class DaemonComponent extends React.Component {
   }
 
   componentDidUpdate() {
-    console.log('didupdate');
-    const vm = this;
     $('#sites-dropdown').dropdown();
     $('#protocol-dropdown').dropdown();
-    $('.ui.dropdown.rights').dropdown();
   }
 
   onChangeProtocol(event) {
-    console.log(event);
     this.setState({ chosenProtocol: event.target.value });
+  }
+
+  onSave(event) {
+    event.preventDefault();
+    const volumesBox = this.refs.volumes;
+    console.log(volumesBox.isFormValid());
+    console.log(volumesBox.state.volumes);
   }
 
   renderSites(sites, defaultSite) {
@@ -72,7 +76,7 @@ class DaemonComponent extends React.Component {
   renderCertificates(item, chosenProtocol) {
       if (item.protocol === 'https' || chosenProtocol === 'https') {
         return(
-        <div className='top-row'>
+        <div className='three fields'>
           <div className='field'>
             <label>CA</label>
             <textarea rows='10' ref='ca' name='ca' defaultValue={item.ca} placeholder='The Certification Authority key Pem file' autoComplete='off'/>
@@ -108,18 +112,19 @@ class DaemonComponent extends React.Component {
                         </div>
                     );
                   } else if (didInvalidate) {
-                    return (<div>No daemon</div>);
+                    return (
+                      <div>No daemon</div>
+                    );
                   } else {
                       return (
                         <div className='daemon-form daemon'>
-                          <div className='tab-content'>
                           <div id='daemon'>
-                            <h1>{item.name}</h1>
+                            <h1><a onClick={()=> this.props.backTo()}><i className='arrow left icon'></i></a>{item.name}</h1>
                             <form className='ui form'>
-                              <div className='top-row'>
-                                <div className='field'>
+                              <div className='two fields'>
+                                <div className='field required'>
                                   <label>
-                                    Name<span className='req'>*</span>
+                                    Name
                                   </label>
                                   <input type='text' ref='name' name='name' defaultValue={item.name} placeholder='A unique name' autoComplete='off' />
                                 </div>
@@ -128,57 +133,58 @@ class DaemonComponent extends React.Component {
                                   <textarea rows='4' ref='description' name='description' defaultValue={item.description} placeholder='A description of the daemon' autoComplete='off'/>
                                 </div>
                               </div>
-                              <div className='top-row'>
-                                <div className='field'>
+                              <div className='two fields'>
+                                <div className='field required'>
                                   <label>
-                                    Site<span className='req'>*</span>
+                                    Site
                                   </label>
                                   {this.renderSites(sites, item.site)}
                                 </div>
-                                <div className='field'>
+                                <div className='field required'>
                                   <label>
-                                    Default data volume<span className='req'>*</span>
+                                    Default data volume
                                   </label>
                                   <input type='text' ref='volume' name='volume' placeholder='volume' defaultValue={item.volume} autoComplete='off'/>
                                 </div>
                               </div>
-                              <div className='top-row'>
-                                <div className='three wide field'>
+                              <div className='five fields'>
+                                <div className='two wide field'>
                                   <div className='large ui label'>Docker daemon :</div>
                                 </div>
-                                <div className='two wide field'>
-                                  <label>Protocol<span className='req'>*</span></label>
+                                <div className='two wide field required'>
+                                  <label>Protocol</label>
                                   <select id='protocol-dropdown' name='protocol' defaultValue={item.protocol} ref='protocol' className='ui fluid dropdown'  onChange={(event) => this.onChangeProtocol(event)}>
                                     <option value=''>Select protocol</option>
                                     <option value='http'>HTTP</option>
                                     <option value='https'>HTTPS</option>
                                   </select>
                                 </div>
-                                <div className='seven wide field'>
-                                  <label>Hostname<span className='req'>*</span></label>
+                                <div className='six wide field required'>
+                                  <label>Hostname</label>
                                   <input type='text' ref='host' name='host' placeholder='Hostname or IP' defaultValue={item.host} autoComplete='off'/>
                                 </div>
-                                <div className='two wide field'>
-                                  <label>Port<span className='req'>*</span></label>
+                                <div className='three wide field required'>
+                                  <label>Port</label>
                                   <input type='number' ref='port' name='port' placeholder='Port' defaultValue={item.port} autoComplete='off'/>
                                 </div>
-                                <div className='two wide field'>
-                                  <label>Timeout<span className='req'>*</span></label>
+                                <div className='three wide field required'>
+                                  <label>Timeout</label>
                                   <input type='number' ref='timeout' name='timeout' placeholder='Timeout' defaultValue={item.timeout} autoComplete='off'/>
                                 </div>
                               </div>
+
                               {this.renderCertificates(item, chosenProtocol)}
+
+
+                              <VolumesBox volumes={item.volumes} ref='volumes'>
+                                <p>These volumes are used to have common volumes mapping on all services deployed on this daemon. You can add / remove / modify volumes mapping when you deploy a new service on a group.</p>
+                              </VolumesBox>
                               <div className='field'>
-                                <VolumesBox volumes={item.volumes}>
-                                  <p>These volumes are used to have common volumes mapping on all services deployed on this daemon. You can add / remove / modify volumes mapping when you deploy a new service on a group.</p>
-                                </VolumesBox>
+                                <a className='ui fluid button button-form' onClick={(event) => this.onSave(event)}>Save</a>
                               </div>
-                              <button type='submit' className='button-form button-block'>Test</button>
                             </form>
                           </div>
-                          </div>
                         </div>
-
                       );
                   }
                 })(isFetching, didInvalidate)}
@@ -193,7 +199,8 @@ DaemonComponent.propTypes = {
   daemonId: React.PropTypes.string,
   sites: React.PropTypes.object,
   fetchDaemon: React.PropTypes.func.isRequired,
-  fetchSites: React.PropTypes.func.isRequired
+  fetchSites: React.PropTypes.func.isRequired,
+  backTo: React.PropTypes.func
 };
 
 // Function to map state to container props
@@ -210,6 +217,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     fetchDaemon : (id) => {dispatch(fetchDaemon(id));},
     fetchSites : () => {dispatch(fetchSitesIfNeeded());},
+    backTo : () => {dispatch(goBack());}
   };
 };
 
