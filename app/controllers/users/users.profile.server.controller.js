0c44bf4662cbb529add5855c283968e157e9665c
Vincent Daniel <vincent.daniel@soprasteria.com>
2017-04-05 15:54:55 +0200

    feat: Removed the loading of groups in the list of users to speed up the page

--- app/controllers/users/users.profile.server.controller.js
+++ app/controllers/users/users.profile.server.controller.js
@@ -30,7 +30,7 @@ exports.delete = function (req, res) {
  * List of Users
  */
 exports.list = function (req, res) {
-    User.find().populate('groups').exec(function (err, users) {
+    User.find().exec(function (err, users) {
         if (err) {
             return res.status(400).send({
                 message: errorHandler.getErrorMessage(err)
