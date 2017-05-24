ab9cd733711928d34b8478dabbb117039ec7f1d9
Leclerc Gwendal <gwendal.leclerc@gmail.com>
2017-05-25 01:01:18 +0200

    feat: make entities.js more readable

--- client/src/modules/utils/entities.js
+++ client/src/modules/utils/entities.js
@@ -7,7 +7,10 @@ import { checkHttpStatus, handleError, parseJSON, parseText } from '../utils/pro
 export const entitySchema = new schema.Entity('all');
 export const entitiesSchema = new schema.Array(entitySchema);
 
-const getConsts = (entitiesName) => {
+//=================================================
+// Generate keys for entities
+//=================================================
+const getKeys = (entitiesName) => {
   const ENTITIES_NAME = entitiesName.toUpperCase();
   const ENTITY_NAME = ENTITIES_NAME.slice(0, -1);
   return {
@@ -27,8 +30,11 @@ const getConsts = (entitiesName) => {
   };
 };
 
+//=================================================
+// Generate constants for entities
+//=================================================
 export const generateEntitiesConstants = (entitiesName) => {
-  const csts = getConsts(entitiesName);
+  const csts = getKeys(entitiesName);
   return {
     [csts.CONST_REQUEST]: csts.CONST_REQUEST,
     [csts.CONST_RECEIVE]: csts.CONST_RECEIVE,
@@ -46,116 +52,78 @@ export const generateEntitiesConstants = (entitiesName) => {
   };
 };
 
+//=================================================
+// Generate actions for entities
+//=================================================
 export const generateEntitiesActions = (entitiesName) => {
-  const csts = getConsts(entitiesName);
+  const csts = getKeys(entitiesName);
+  const requestAll = () => ({ type: csts.CONST_REQUEST });
+  const receiveSome = (response) => ({ type: csts.CONST_RECEIVE, response });
+  const requestOne = (id) => ({ type: csts.CONST_REQUEST_ENTITY, id });
+  const receiveOne = (response) => ({ type: csts.CONST_RECEIVE_ENTITY, response });
+  const requestSave = (entity) => ({ type: csts.CONST_SAVE_ENTITY, entity });
+  const saved = (response) => ({ type: csts.CONST_ENTITY_SAVED, response });
+  const requestDelete = (id) => ({ type: csts.CONST_DELETE_ENTITY, id });
+  const deleted = (id) => ({ type: csts.CONST_ENTITY_DELETED, id });
+  const changeFilter = (filterValue) => ({ type: csts.CHANGE_FILTER, filterValue });
+  const invalidRequest = (error) => ({
+    type: csts.CONST_INVALID,
+    title: `Error on ${entitiesName.toLowerCase()} API`,
+    message: error,
+    level: 'error'
+  });
+  const invalidRequestEntity = (entity) => (error) => {
+    const entityName = entitiesName.toLowerCase().slice(0, -1);
+    const title = entity.title || entity.name || entity.username || entity.id;
+    return {
+      type: csts.CONST_INVALID_ENTITY,
+      title: `Cannot fetch ${entityName} ${title}`,
+      message: error,
+      level: 'error',
+      entity
+    };
+  };
+  const invalidSaveEntity = (entity) => (error) => {
+    const entityName = entitiesName.toLowerCase().slice(0, -1);
+    const title = entity.title || entity.username || entity.name && (entity.name.raw || entity.name);
+    return {
+      type: csts.CONST_INVALID_SAVE_ENTITY,
+      title: `Cannot save ${entityName} ${title}`,
+      message: error,
+      level: 'error',
+      entity
+    };
+  };
+  const invalidDeleteEntity = (entity) => (error) => {
+    const entityName = entitiesName.toLowerCase().slice(0, -1);
+    const title = entity.title || entity.username || entity.name && (entity.name.raw || entity.name);
+    return {
+      type: csts.CONST_INVALID_DELETE_ENTITY,
+      title: `Cannot delete ${entityName} ${title}`,
+      message: error,
+      level: 'error',
+      entity
+    };
+  };
   return {
-    requestAll: () => {
-      return { type: csts.CONST_REQUEST };
-    },
-    receiveSome: (response) => {
-      return {
-        type: csts.CONST_RECEIVE,
-        response,
-        receivedAt: Date.now()
-      };
-    },
-    invalidRequest: (error) => {
-      return {
-        type: csts.CONST_INVALID,
-        title: `Error on ${entitiesName.toLowerCase()} API`,
-        message: error,
-        level: 'error'
-      };
-    },
-    requestOne: (id) => {
-      return {
-        type: csts.CONST_REQUEST_ENTITY,
-        id
-      };
-    },
-    receiveOne: (response) => {
-      return {
-        type: csts.CONST_RECEIVE_ENTITY,
-        response
-      };
-    },
-    invalidRequestEntity: (entity) => (error) => {
-      const entityName = entitiesName.toLowerCase().slice(0, -1);
-      const title = entity.title || entity.name || entity.username || entity.id;
-      return {
-        type: csts.CONST_INVALID_ENTITY,
-        title: `Cannot fetch ${entityName} ${title}`,
-        message: error,
-        level: 'error',
-        entity
-      };
-    },
-    requestSave: (entity) => {
-      return {
-        type: csts.CONST_SAVE_ENTITY,
-        entity
-      };
-    },
-    saved: (response) => {
-      return {
-        type: csts.CONST_ENTITY_SAVED,
-        response
-      };
-    },
-    invalidSaveEntity: (entity) => (error) => {
-      const entityName = entitiesName.toLowerCase().slice(0, -1);
-      const title = entity.title || entity.username || entity.name && (entity.name.raw || entity.name);
-      return {
-        type: csts.CONST_INVALID_SAVE_ENTITY,
-        title: `Cannot save ${entityName} ${title}`,
-        message: error,
-        level: 'error',
-        entity
-      };
-    },
-    requestDelete: (id) => {
-      return {
-        type: csts.CONST_DELETE_ENTITY,
-        id
-      };
-    },
-    deleted: (id) => {
-      return {
-        type: csts.CONST_ENTITY_DELETED,
-        id,
-        receivedAt: Date.now()
-      };
-    },
-    invalidDeleteEntity: (entity) => (error) => {
-      const entityName = entitiesName.toLowerCase().slice(0, -1);
-      const title = entity.title || entity.username || entity.name && (entity.name.raw || entity.name);
-      return {
-        type: csts.CONST_INVALID_DELETE_ENTITY,
-        title: `Cannot delete ${entityName} ${title}`,
-        message: error,
-        level: 'error',
-        entity
-      };
-    },
-    changeFilter: (filterValue) => {
-      return {
-        type: csts.CHANGE_FILTER,
-        filterValue
-      };
-    },
+    requestAll, receiveSome, invalidRequest, requestOne, receiveOne, invalidRequestEntity,
+    requestSave, saved, invalidSaveEntity, requestDelete, deleted, invalidDeleteEntity, changeFilter,
   };
 };
 
-const generateEntitiesFilterValue = (csts) => (state = '', action) => {
+//=================================================
+// Generate reducers for entities
+//=================================================
+const filterReducer = (csts) => (filterValue = '', action) => {
   switch (action.type) {
     case csts.CHANGE_FILTER:
       return action.filterValue;
     default:
-      return state;
+      return filterValue;
   }
 };
 
-const generateEntitiesIsFetching = (csts) => (state = false, action) => {
+const isFetchingReducer = (csts) => (isFetching = false, action) => {
   switch (action.type) {
     case csts.CONST_REQUEST:
       return true;
@@ -164,72 +132,70 @@ const generateEntitiesIsFetching = (csts) => (state = false, action) => {
     case LOCATION_CHANGE:
       return false;
     default:
-      return state;
+      return isFetching;
   }
 };
 
-const generateEntitiesItems = (csts) => (state = {}, action) => {
+const itemsReducer = (csts) => (items = {}, action) => {
   switch (action.type) {
     case csts.CONST_RECEIVE:
     case csts.CONST_RECEIVE_ENTITY:
     case csts.CONST_ENTITY_SAVED:
       if (!action.response.entities) {
-        return state;
+        return items;
       }
       return {
-        ...state,
+        ...items,
         ...action.response.entities.all
       };
     case csts.CONST_REQUEST_ENTITY: {
-      const { [action.id]: item, ...restState } = state;
+      const { [action.id]: item, ...restState } = items;
       return { [action.id]: { ...item, isFetching: true }, ...restState };
     }
     case csts.CONST_SAVE_ENTITY: {
-      const { [action.entity.id]: item, ...restState } = state;
+      const { [action.entity.id]: item, ...restState } = items;
       return { [action.entity.id]: { ...item, isFetching: true }, ...restState };
     }
     case csts.CONST_DELETE_ENTITY: {
-      const { [action.id]: item, ...restState } = state;
+      const { [action.id]: item, ...restState } = items;
       return { [action.id]: { ...item, isFetching: true }, ...restState };
     }
     case csts.CONST_INVALID_ENTITY:
     case csts.CONST_INVALID_SAVE_ENTITY:
     case csts.CONST_INVALID_DELETE_ENTITY: {
-      const { [action.entity.id]: item, ...restState } = state;
+      const { [action.entity.id]: item, ...restState } = items;
       return { [action.entity.id]: { ...item, isFetching: false }, ...restState };
     }
     case csts.CONST_ENTITY_DELETED: {
-      const { [action.id]: _, ...newState } = state;
+      const { [action.id]: _, ...newState } = items;
       return newState;
     }
     default:
-      return state;
+      return items;
   }
 };
 
-
-
-const generateEntitiesList = (csts) => (state = [], action) => {
+const listReducer = (csts) => (list = [], action) => {
   switch (action.type) {
     case csts.CONST_RECEIVE:
       if (!action.response.result) {
-        return state;
+        return list;
       }
-      return [...state, ...action.response.result];
+      return [...list, ...action.response.result];
     case csts.CONST_RECEIVE_ENTITY:
     case csts.CONST_ENTITY_SAVED:
       if (!action.response.result) {
-        return state;
+        return list;
       }
-      return [...state, action.response.result];
+      return [...list, action.response.result];
     case csts.CONST_ENTITY_DELETED:
-      return state.filter(id => id != action.id);
+      return list.filter(id => id != action.id);
     default:
-      return state;
+      return list;
   }
 };
 
-const generateEntityIsFetching = (csts) => (state = false, action) => {
+const selectedIsFetchingReducer = (csts) => (isFetching = false, action) => {
   switch (action.type) {
     case csts.CONST_REQUEST_ENTITY:
     case csts.CONST_SAVE_ENTITY:
@@ -244,11 +210,11 @@ const generateEntityIsFetching = (csts) => (state = false, action) => {
     case LOCATION_CHANGE:
       return false;
     default:
-      return state;
+      return isFetching;
   }
 };
 
-const generateEntityId = (csts) => (state = '', action) => {
+const selectedIdReducer = (csts) => (id = '', action) => {
   switch (action.type) {
     case csts.CONST_REQUEST_ENTITY:
       return action.id || '';
@@ -262,25 +228,28 @@ const generateEntityId = (csts) => (state = '', action) => {
     case csts.CONST_INVALID_DELETE_ENTITY:
       return action.entity.id || '';
     default:
-      return state;
+      return id;
   }
 };
 
 export const generateEntitiesReducer = (entitiesName) => {
-  const csts = getConsts(entitiesName);
+  const csts = getKeys(entitiesName);
   const selected = combineReducers({
-    isFetching: generateEntityIsFetching(csts),
-    id: generateEntityId(csts),
+    isFetching: selectedIsFetchingReducer(csts),
+    id: selectedIdReducer(csts),
   });
   return combineReducers({
-    filterValue: generateEntitiesFilterValue(csts),
-    isFetching: generateEntitiesIsFetching(csts),
-    items: generateEntitiesItems(csts),
-    list: generateEntitiesList(csts),
+    filterValue: filterReducer(csts),
+    isFetching: isFetchingReducer(csts),
+    items: itemsReducer(csts),
+    list: listReducer(csts),
     selected,
   });
 };
 
+//=================================================
+// Generate thunks for entities
+//=================================================
 export const generateEntitiesThunks = (entitiesName) => {
   const Actions = generateEntitiesActions(entitiesName);
   const fetchAllFunc = () => {
@@ -361,10 +330,5 @@ export const generateEntitiesThunks = (entitiesName) => {
         });
     };
   };
-  return {
-    fetchAll: fetchAllFunc,
-    fetch: fetchFunc,
-    save: saveFunc,
-    delete: deleteFunc
-  };
+  return { fetchAll: fetchAllFunc, fetch: fetchFunc, save: saveFunc, delete: deleteFunc };
 };
